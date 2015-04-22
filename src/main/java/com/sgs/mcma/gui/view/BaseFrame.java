9d3076ef32fc93ccb8cc0f39c9a8499970ee7d4f
longlostbro <longlostbro@gmail.com>
2015-04-21 20:02:41 -0600

    added box layout

--- src/main/java/com/sgs/mcma/gui/view/BaseFrame.java
+++ src/main/java/com/sgs/mcma/gui/view/BaseFrame.java
@@ -2,15 +2,28 @@ package com.sgs.mcma.gui.view;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javafx.scene.control.Alert;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JSplitPane;
 import javax.swing.JTabbedPane;
 
 public class BaseFrame extends JFrame {
-	
+	private static BaseFrame instance;
+	public static BaseFrame Instance(){
+		return instance;
+	}
 	public BaseFrame(String title){
+		instance = this;
 		Initialize(title);
 	}
 
@@ -26,7 +39,47 @@ public class BaseFrame extends JFrame {
 		mainPanel.setLayout(new BorderLayout());
 		
 		JTabbedPane tabs = new JTabbedPane();
-		tabs.addTab("Tab 1", new JPanel());
+		
+		JPanel tab1 = new JPanel();
+		tab1.setLayout(new BorderLayout());
+		
+		JButton startButton = new JButton("START");
+		startButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				startServer();
+			}
+
+			private void startServer() {
+				JOptionPane.showMessageDialog(BaseFrame.Instance(), "server started");
+			}
+		});
+		
+		JButton stopButton = new JButton("STOP");
+		stopButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				stopServer();
+			}
+
+			private void stopServer() {
+				JOptionPane.showMessageDialog(BaseFrame.Instance(), "server started");
+			}
+		});
+		
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.PAGE_AXIS));
+		buttonPanel.add(Box.createVerticalGlue());
+		Box buttonBox = Box.createHorizontalBox();
+		buttonBox.add(Box.createHorizontalGlue());
+		buttonBox.add(startButton);
+		buttonBox.add(Box.createHorizontalStrut(5));
+		buttonBox.add(stopButton);
+		buttonBox.add(Box.createHorizontalGlue());
+		
+		buttonPanel.add(buttonBox);
+		
+		tab1.add(buttonPanel);
+		
+		tabs.addTab("Tab 1", tab1);
 		tabs.addTab("Tab 2", new JPanel());
 		tabs.addTab("Tab 3", new JPanel());
 		
