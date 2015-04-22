8dff45c76d8549d6d1ecf9c337a654c4a8184895
longlostbro <longlostbro@gmail.com>
2015-04-21 19:31:51 -0600

    added tabs to frame.

--- src/main/java/com/sgs/mcma/gui/view/BaseFrame.java
+++ src/main/java/com/sgs/mcma/gui/view/BaseFrame.java
@@ -1,8 +1,12 @@
 package com.sgs.mcma.gui.view;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JSplitPane;
+import javax.swing.JTabbedPane;
 
 public class BaseFrame extends JFrame {
 	
@@ -18,8 +22,17 @@ public class BaseFrame extends JFrame {
 	}
 
 	private void populateFrame() {
-		// TODO Auto-generated method stub
+		JPanel mainPanel = new JPanel();
+		mainPanel.setLayout(new BorderLayout());
 		
+		JTabbedPane tabs = new JTabbedPane();
+		tabs.addTab("Tab 1", new JPanel());
+		tabs.addTab("Tab 2", new JPanel());
+		tabs.addTab("Tab 3", new JPanel());
+		
+		mainPanel.add(tabs, BorderLayout.CENTER);
+		
+		this.add(mainPanel);
 	}
 
 }
