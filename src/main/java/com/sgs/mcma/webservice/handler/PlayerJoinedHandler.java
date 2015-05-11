23afe88d7ee57c1b87d6f26f967534c350373e01
Russell McDonnell <Russell.McDonnell@wavetronix.com>
2015-05-11 12:33:39 -0600

    Suppressed warnings and changed pom file.

--- src/main/java/com/sgs/mcma/webservice/handler/PlayerJoinedHandler.java
+++ src/main/java/com/sgs/mcma/webservice/handler/PlayerJoinedHandler.java
@@ -17,6 +17,7 @@ import com.sgs.mcma.webservice.ServerFacade;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
+@SuppressWarnings("restriction")
 public class PlayerJoinedHandler implements HttpHandler {
 
 	private Logger logger = Logger.getLogger("MCMA");
