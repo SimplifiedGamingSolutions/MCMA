6c9700aac54981f9b9e7770fb852c909ee607cee
David Taylor <dtaylor@wavetronix.com>
2015-05-13 14:52:43 -0600

    created class for popup

---
+++ src/main/java/com/sgs/mcma/gui/view/PlayerCommandMenu.java
@@ -0,0 +1,100 @@
+package com.sgs.mcma.gui.view;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+
+import javax.swing.ImageIcon;
+import javax.swing.JList;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JPopupMenu;
+import javax.swing.UIManager;
+
+import com.sgs.mcma.gui.view.console.ConsolePane;
+
+public class PlayerCommandMenu extends JPopupMenu {
+	PlayerCommandMenu menu = this;
+	
+	public PlayerCommandMenu()
+	{
+		addCommand("achievement", "achievement");
+		addCommand("ban", "ban player");
+		addCommand("ban-ip", "ban-ip player");
+		addCommand("kill", "kill player");
+		addCommand("clear", "todo");
+		addCommand("deop", "todo");
+		addCommand("difficulty", "todo");
+		addCommand("effect", "todo");
+		addCommand("enchant", "todo");
+		addCommand("gamemode", "todo");
+		addCommand("give", "todo");
+		addCommand("kick", "todo");
+		addCommand("kill", "todo");
+		addCommand("op", "todo");
+		addCommand("pardon", "todo");
+		addCommand("particle", "todo");
+		addCommand("playsound", "todo");
+		addCommand("replaceitem", "todo");
+		addCommand("scoreboard", "todo");
+		addCommand("setidletimeout", "todo");
+		addCommand("spreadplayers", "todo");
+		addCommand("stats", "todo");
+		addCommand("tell", "todo");
+		addCommand("tellraw", "todo");
+		addCommand("testfor", "todo");
+		addCommand("title", "todo");
+		addCommand("tp", "todo");
+		addCommand("trigger", "todo");
+		addCommand("whitelist", "todo");
+		addCommand("xp", "todo");
+
+	}
+	public void mouseEntered(MouseEvent event){
+		JMenuItem jmi = (JMenuItem) event.getSource();
+		jmi.setBackground(UIManager.getColor    ("MenuItem.selectionBackground"));
+	    jmi.setForeground(UIManager.getColor("MenuItem.selectionForeground"));
+	}
+	
+	public void addCommand(String title, String command){
+		JMenuItem temp = new JMenuItem(title);
+		this.add(temp);
+		temp.addActionListener(new commandActionListener(command, title));
+	
+	}
+	
+	private class commandActionListener implements ActionListener{
+		private String command;
+		private String title;
+		
+		ImageIcon img = new ImageIcon("Resources\\give.png");
+
+		public commandActionListener(String command, String title){
+			this.command = command;
+			this.title = title;
+			menu.setVisible(false);
+			
+		}
+		@SuppressWarnings("static-access")
+		public void actionPerformed(ActionEvent e) {
+			JList<String> playerList = PlayerListPanel.instance.playerList;
+			ConsolePane console = PlayerListPanel.instance.console;
+			if(command.equals("achievement")){
+				String [] options = {"Give Player","Take Player"};
+				JOptionPane optionPane = new JOptionPane();
+				//optionPane.setLocation(x,y);
+				//BaseFrame.instance.getLocation().getX() + BaseFrame.instance.getWidth()/2;
+				
+				menu.setVisible(false);
+				optionPane.showOptionDialog(BaseFrame.instance, "Give or Take Achievement from "+playerList.getSelectedValue(), title.toUpperCase(), JOptionPane.YES_NO_CANCEL_OPTION,JOptionPane.QUESTION_MESSAGE,img, options,options[0]);
+				
+				
+			}else{			
+				String player = playerList.getSelectedValue();
+				console.sendCommand(command.replace("player", player));
+				menu.setVisible(false);
+			}
+			
+		}
+	}
+}
--- src/main/java/com/sgs/mcma/gui/view/PlayerListPanel.java
+++ src/main/java/com/sgs/mcma/gui/view/PlayerListPanel.java
@@ -29,11 +29,11 @@ import com.sgs.mcma.gui.view.console.ConsolePane;
 
 @SuppressWarnings("serial")
 public class PlayerListPanel extends JPanel{
-	private JList<String> playerList = new JList<String>();
-	public static JPopupMenu popup;
-	ConsolePane console;
+	public JList<String> playerList = new JList<String>();
+	public ConsolePane console;
 	DefaultListModel<String> playerListModel;
-	private static PlayerListPanel instance;
+	public static PlayerListPanel instance;
+	private PlayerCommandMenu popup;
 
 	public PlayerListPanel(ConsolePane c, DefaultListModel<String> plm) {
 		instance = this;
@@ -88,92 +88,6 @@ public class PlayerListPanel extends JPanel{
 				playerList.clearSelection();
 			}
 		}
-	}
-	
-	private class PlayerCommandMenu extends JPopupMenu{
-		PlayerCommandMenu menu = this;
-		
-		public PlayerCommandMenu()
-		{
-			addCommand("achievement", "achievement");
-			addCommand("ban", "ban player");
-			addCommand("ban-ip", "ban-ip player");
-			addCommand("kill", "kill player");
-			addCommand("clear", "todo");
-			addCommand("deop", "todo");
-			addCommand("difficulty", "todo");
-			addCommand("effect", "todo");
-			addCommand("enchant", "todo");
-			addCommand("gamemode", "todo");
-			addCommand("give", "todo");
-			addCommand("kick", "todo");
-			addCommand("kill", "todo");
-			addCommand("op", "todo");
-			addCommand("pardon", "todo");
-			addCommand("particle", "todo");
-			addCommand("playsound", "todo");
-			addCommand("replaceitem", "todo");
-			addCommand("scoreboard", "todo");
-			addCommand("setidletimeout", "todo");
-			addCommand("spreadplayers", "todo");
-			addCommand("stats", "todo");
-			addCommand("tell", "todo");
-			addCommand("tellraw", "todo");
-			addCommand("testfor", "todo");
-			addCommand("title", "todo");
-			addCommand("tp", "todo");
-			addCommand("trigger", "todo");
-			addCommand("whitelist", "todo");
-			addCommand("xp", "todo");
-
-		}
-		public void mouseEntered(MouseEvent event){
-			JMenuItem jmi = (JMenuItem) event.getSource();
-			jmi.setBackground(UIManager.getColor    ("MenuItem.selectionBackground"));
-		    jmi.setForeground(UIManager.getColor("MenuItem.selectionForeground"));
-		}
-		
-		public void addCommand(String title, String command){
-			JMenuItem temp = new JMenuItem(title);
-			this.add(temp);
-			temp.addActionListener(new commandActionListener(command, title));
-		
-		}
-		
-		private class commandActionListener implements ActionListener{
-			private String command;
-			private String title;
-			
-			ImageIcon img = new ImageIcon("Resources\\give.png");
-
-			public commandActionListener(String command, String title){
-				this.command = command;
-				this.title = title;
-				menu.setVisible(false);
-				
-			}
-			public void actionPerformed(ActionEvent e) {
-				if(command.equals("achievement")){
-					String [] options = {"Give Player","Take Player"};
-					JOptionPane optionPane = new JOptionPane();
-					//optionPane.setLocation(x,y);
-					//BaseFrame.instance.getLocation().getX() + BaseFrame.instance.getWidth()/2;
-					
-					menu.setVisible(false);
-					optionPane.showOptionDialog(BaseFrame.instance, "Give or Take Achievement from "+playerList.getSelectedValue(), title.toUpperCase(), JOptionPane.YES_NO_CANCEL_OPTION,JOptionPane.QUESTION_MESSAGE,img, options,options[0]);
-					
-					
-				}else{			
-					String player = playerList.getSelectedValue();
-					console.sendCommand(command.replace("player", player));
-					menu.setVisible(false);
-				}
-				
-			}
-		}
-		
-
-		
 	}
 	
 }
