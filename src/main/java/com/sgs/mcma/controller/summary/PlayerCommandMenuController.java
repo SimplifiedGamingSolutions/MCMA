15e162dd53b218c68dd48d9fff9335ce6de51834
Russell McDonnell <Russell.McDonnell@wavetronix.com>
2015-05-27 17:00:22 -0600

    Got gamemode and difficulty working as player commands. Also added some code that will check if the server is running or if the exit out of a command.

--- src/main/java/com/sgs/mcma/controller/summary/PlayerCommandMenuController.java
+++ src/main/java/com/sgs/mcma/controller/summary/PlayerCommandMenuController.java
@@ -15,6 +15,8 @@ public class PlayerCommandMenuController
 
 	public static void commandPressed(JList<String> playerList, ConsolePane console, String command, String title, PlayerCommandMenu menu)
 	{
+		if(!console.isRunning())
+			return;
 		String result;
 		String player = playerList.getSelectedValue();
 		menu.setVisible(false);
@@ -25,45 +27,59 @@ public class PlayerCommandMenuController
 			ImageIcon img = new ImageIcon(ClassLoader.getSystemResource("Resources/Images/Achievements/give.png"));
 			int n = JOptionPane.showOptionDialog(BaseFrame.instance, "Give or Take Achievement from " + playerList.getSelectedValue(), title.toUpperCase(), JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, img, options, options[0]);
 			
-			//if they say give
-			if(n==JOptionPane.YES_NO_OPTION)
-			{	
-				String instructions = "Choose an Achievement below to give to "+playerList.getSelectedValue();
-				
-				ItemSelectionDialog dialog = new ItemSelectionDialog("Achievements",instructions);
-				dialog.addAchievements();
-				dialog.setLocationRelativeTo(BaseFrame.instance);
-				dialog.setModal(true);
-				dialog.setVisible(true);
-				
-				result = dialog.getResult();
-				console.sendCommand("achievement give achievement."+result+" "+player);
-			}
-			//if they say take
-			else{
-				String instructions = "Choose an Achievement below to take from "+playerList.getSelectedValue();
-				
-				ItemSelectionDialog dialog = new ItemSelectionDialog("Achievements",instructions);
-				dialog.addAchievements();
-				dialog.setLocationRelativeTo(BaseFrame.instance);
-				dialog.setModal(true);
-				dialog.setVisible(true);
-				
-				result = dialog.getResult();
-				console.sendCommand("achievement take achievement."+result+" "+player);
+			if(n != -1)
+			{
+				//if they say give
+				if(n==JOptionPane.YES_NO_OPTION)
+				{	
+					String instructions = "Choose an Achievement below to give to "+playerList.getSelectedValue();
+					
+					ItemSelectionDialog dialog = new ItemSelectionDialog("Achievements",instructions);
+					dialog.addAchievements();
+					dialog.setLocationRelativeTo(BaseFrame.instance);
+					dialog.setModal(true);
+					dialog.setVisible(true);
+					
+					result = dialog.getResult();
+					console.sendCommand("achievement give achievement."+result+" "+player);
+				}
+				//if they say take
+				else{
+					String instructions = "Choose an Achievement below to take from "+playerList.getSelectedValue();
+					
+					ItemSelectionDialog dialog = new ItemSelectionDialog("Achievements",instructions);
+					dialog.addAchievements();
+					dialog.setLocationRelativeTo(BaseFrame.instance);
+					dialog.setModal(true);
+					dialog.setVisible(true);
+					
+					result = dialog.getResult();
+					console.sendCommand("achievement take achievement."+result+" "+player);
+				}
 			}
 			
-		} else//another player command
+		} else if(command.equals("difficulty"))
+		{
+			String[] options ={	"Peaceful", "Easy", "Normal", "Hard"};
+			//ImageIcon img = new ImageIcon(ClassLoader.getSystemResource("Resources/Images/Achievements/give.png")); This is if you wan to add an image					 goes here
+			int n = JOptionPane.showOptionDialog(BaseFrame.instance, "Select Difficulty", title.toUpperCase(), JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
+			if(n != -1)
+				console.sendCommand("difficulty " + n);			
+		}else if(command.equals("gamemode"))
+		{
+			String[] options ={	"Survival", "Creative", "Adventure", "Spectator"};
+			//ImageIcon img = new ImageIcon(ClassLoader.getSystemResource("Resources/Images/Achievements/give.png")); This is if you wan to add an image					 goes here
+			int n = JOptionPane.showOptionDialog(BaseFrame.instance, "Select Gamemode for " + playerList.getSelectedValue(), title.toUpperCase(), JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
+			if(n != -1)
+				console.sendCommand("gamemode " + n + " " + player);			
+		}
+		else//another player command
 		{
 			
 			console.sendCommand(command.replace("[player]", player));
 			menu.setVisible(false);
 		}
 		//after all the commands
-		
-		
-		
-		
 	}
 
 }
